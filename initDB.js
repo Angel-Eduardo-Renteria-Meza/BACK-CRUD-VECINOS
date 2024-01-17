f94457c654f4187cc0d44aa7bea2c2d2acc2e83d
Angel-Eduardo-Renteria-Meza <129317548+Angel-Eduardo-Renteria-Meza@users.noreply.github.com>
2024-01-17 15:27:37 -0600

    eliminacion de uri

--- initDB.js
+++ initDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 module.exports = initDB = () => {
-mongoose.connect("mongodb://PruebaTecnica:Prueba123@198.199.66.16:27017/prueba_tecnica")
+mongoose.connect("")
 .then(console.log("base de datos conectada"))
 .catch(err => console.log(err))
 }
